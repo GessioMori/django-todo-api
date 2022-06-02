b7cc2367eb96e5d02daa2399ab287ce2bfbaa45e
Gessio Mori Neto <gessiomorin@gmail.com>
2022-06-02 09:20:14 -0300

    User registration

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(max_length=1000)
+    is_completed = models.BooleanField(default=False)
+    owner = models.ForeignKey(
+        'auth.User', related_name='todos', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['created_at']
