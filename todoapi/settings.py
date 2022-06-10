9fa345780a9d8e9fd45ae388d7a431c38470f5ba
Gessio Mori Neto <gessiomorin@gmail.com>
2022-06-09 23:26:22 -0300

    Installed CORS middleware

--- todoapi/settings.py
+++ todoapi/settings.py
@@ -36,8 +36,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
-
 ]
 
 ROOT_URLCONF = 'todoapi.urls'
